0dc37a2d8eb508b303020529bfefaadcfb237401
bage2014 <893542907@qq.com>
2020-02-10 22:47:31 +0800

    circle image view

---
+++ app/src/main/java/com/bage/tutorials/view/IconView.java
@@ -0,0 +1,18 @@
+package com.bage.tutorials.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class IconView extends net.steamcrafted.materialiconlib.MaterialIconView {
+    public IconView(Context context) {
+        super(context);
+    }
+
+    public IconView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IconView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}

3045d0cbebf31f2011afc61903bc818ce8ef1f2c
bage2014 <893542907@qq.com>
2020-05-27 23:54:53 +0800

    icon

--- app/src/main/java/com/bage/tutorials/ui/tv/TVPlayingActivity.java
+++ app/src/main/java/com/bage/tutorials/ui/tv/TVPlayingActivity.java
@@ -134,9 +134,9 @@ public class TVPlayingActivity extends AppCompatActivity {
 
     private void toggleFullscreenImage() {
         if (TVPlayingActivity.isFullscreen) {
-            fullScreenImageView.setImageResource(R.drawable.exo_icon_fullscreen_exit);
+            fullScreenImageView.setImageResource(R.drawable.ic_exo_fullscreen_exit);
         } else {
-            fullScreenImageView.setImageResource(R.drawable.exo_icon_fullscreen_enter);
+            fullScreenImageView.setImageResource(R.drawable.ic_exo_fullscreen_enter);
         }
     }
 

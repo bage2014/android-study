17d559e85d7d4cf7ddab3f080d8e9bbf436cf48c
bage2014 <893542907@qq.com>
2020-06-17 23:37:31 +0800

    weather adapter

--- app/src/main/java/com/bage/tutorials/ui/weather/WeatherRecyclerViewAdapter.java
+++ app/src/main/java/com/bage/tutorials/ui/weather/WeatherRecyclerViewAdapter.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -38,8 +39,21 @@ public class WeatherRecyclerViewAdapter extends RecyclerView.Adapter<WeatherRecy
 
     @Override
     public void onBindViewHolder(WeatherRecyclerViewAdapter.MyViewHolder holder, int position) {
-        holder.tvDay.setText(list.get(position).getDay());
+        holder.tvDay.setText(list.get(position).getDate());
         holder.tvWeather.setText(list.get(position).getTem());
+        // https://dss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/aladdin/img/new_weath/icon/1.png
+        //
+        int resId = 0;
+        if ("yin".endsWith(list.get(position).getWeaImg())) {
+            resId = R.drawable.win;
+        } else if ("yun".endsWith(list.get(position).getWeaImg())) {
+            resId = R.drawable.win;
+        } else if ("yu".endsWith(list.get(position).getWeaImg())) {
+            resId = R.drawable.rain;
+        } else if ("qing".endsWith(list.get(position).getWeaImg())) {
+            resId = R.drawable.sun;
+        }
+        holder.tvIcon.setImageDrawable(context.getResources().getDrawable(resId));
     }
 
     @Override
@@ -50,11 +64,13 @@ public class WeatherRecyclerViewAdapter extends RecyclerView.Adapter<WeatherRecy
     class MyViewHolder extends RecyclerView.ViewHolder {
         TextView tvDay;
         TextView tvWeather;
+        ImageButton tvIcon;
 
         MyViewHolder(View itemView) {
             super(itemView);
             tvDay = itemView.findViewById(R.id.item_tv_day);
             tvWeather = itemView.findViewById(R.id.item_tv_weather);
+            tvIcon = itemView.findViewById(R.id.item_tv_icon);
 
             //添加点击事件
             itemView.setOnClickListener(new View.OnClickListener() {
